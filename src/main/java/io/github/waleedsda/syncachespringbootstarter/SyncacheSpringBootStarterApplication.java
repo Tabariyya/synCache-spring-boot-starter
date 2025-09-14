5af5adbe63af5a168560bff53baf87e53a3001ac
waleedshanaa <waleed_shanaa@icloud.com>
2025-09-15 00:57:36 +0300

    init

---
+++ src/main/java/io/github/waleedsda/syncachespringbootstarter/SyncacheSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.waleedsda.syncachespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncacheSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncacheSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/waleedsda/syncachespringbootstarter/SyncacheSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.waleedsda.syncachespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncacheSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
